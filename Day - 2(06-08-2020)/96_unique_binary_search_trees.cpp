e39416e429f84ac3d5e774888e4777421142a7a1
suryatejabandlamudi <suryatejabandlamudi@gmail.com>
2020-08-06 23:53:19 +0530

    Completed 06th Aug problems

--- Day - 2(06-08-2020)/96_unique_binary_search_trees.cpp
+++ Day - 2(06-08-2020)/96_unique_binary_search_trees.cpp
@@ -1,2 +1,23 @@
 // https://leetcode.com/problems/unique-binary-search-trees/
 
+class Solution {
+public:
+    int numTrees(int n) {
+        
+        int arr[n+1];
+        arr[0] = 1;
+        arr[1] = 1;
+        for(int i = 2; i<=n; i++)
+        {
+            arr[i] = 0;
+            for(int j = 1; j<=i; j++)
+            {
+                arr[i] += arr[i-j] * arr[j-1];
+            }
+            
+        }
+        
+        return arr[n];
+        
+    }
+};
