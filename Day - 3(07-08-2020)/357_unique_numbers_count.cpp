7c0765b7f823c70de1217f18eb3707de2a777515
suryatejabandlamudi <suryatejabandlamudi@gmail.com>
2020-08-07 23:39:40 +0530

    5 Medium problems Done

--- Day - 3(07-08-2020)/357_unique_numbers_count.cpp
+++ Day - 3(07-08-2020)/357_unique_numbers_count.cpp
@@ -1,2 +1,25 @@
 // https://leetcode.com/problems/count-numbers-with-unique-digits/
 
+class Solution {
+public:
+    int countNumbersWithUniqueDigits(int n) {
+        
+        if(n==0)
+            return 1;
+        
+        int res = 0;
+        int memory[n+1];
+        memory[0] = 0;
+        memory[1] = 10;
+        res += memory[1];
+        for(int i = 2; i<=n; i++)
+        {
+            if(i==2)
+                memory[i] = (memory[i-1]-1) * (10-i+1);
+            else
+                 memory[i]= (memory[i-1] * (10-i+1));
+            res += memory[i];
+        }
+        return res;
+    }
+};
--- Day - 3(07-08-2020)/740_delete_and_earn.cpp
+++ Day - 3(07-08-2020)/740_delete_and_earn.cpp
@@ -1,2 +1,30 @@
 // https://leetcode.com/problems/delete-and-earn/
 
+class Solution {
+public:
+    int deleteAndEarn(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        
+        int i = 0;
+        int cur = 0;
+        int prev = 0;
+        int prev2 = 0;        
+        int startIndex = 0;
+        
+        while(i < n){
+            cur = nums[i];
+            startIndex = i - 1;
+            while(i < n - 1 && nums[i] == nums[i+1]) cur += nums[i++];  
+            
+            if(startIndex >= 0 && nums[i] > nums[startIndex] + 1) cur += prev;
+            else cur = max(cur + prev2, prev);            
+            
+            prev2 = prev;
+            prev = cur;
+            i++;
+        }    
+        return cur;     
+    }
+    
+};
