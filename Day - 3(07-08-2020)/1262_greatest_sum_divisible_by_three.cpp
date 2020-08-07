9beb83de5c38f77008cc1465b80fcdef44dcb1d3
suryatejabandlamudi <suryatejabandlamudi@gmail.com>
2020-08-07 21:38:15 +0530

    completed 3 problems

--- Day - 3(07-08-2020)/1262_greatest_sum_divisible_by_three.cpp
+++ Day - 3(07-08-2020)/1262_greatest_sum_divisible_by_three.cpp
@@ -1,2 +1,18 @@
 // https://leetcode.com/problems/greatest-sum-divisible-by-three/
 
+class Solution {
+public:
+    int maxSumDivThree(vector<int>& nums) {
+        
+        vector<int> memory(3,0);
+        vector<int> memory_copy(3,0);
+        
+        for(int i=0; i<nums.size(); i++)
+        {
+            memory_copy = memory;
+            for(int sum: memory_copy)
+                memory[(nums[i] + sum) % 3] = max((nums[i]+sum) , memory[(nums[i] + sum) % 3]);
+        }
+        return memory[0]; 
+    }
+};
